46811f6ae625ed62447abce5ffcc2adcb87cd7eb
Andrew Vang <andrew-vang-18@cnu.edu>
2021-02-19 15:04:03 -0500

    Monolithic file

---
+++ includes/fileio.h
@@ -0,0 +1,17 @@
+#ifndef FILEIOH
+#define FILEIOH
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "constants.h"
+
+//attempt to open file 'filename' and read in all data
+//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+int loadData(const std::string filename, std::vector<process> &myProcesses);
+
+//attempt to create or open file 'filename' to write all data to
+//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+int saveData(const std::string filename, std::vector<process> &myProcess);
+
+#endif /* FILEIOH */
